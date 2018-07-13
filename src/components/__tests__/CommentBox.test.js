83d5055d9b79100f0166f78d09e1afafdbf21425
abe menashy <abestbet1@gmail.com>
2018-07-12 22:17:53 -0700

    after each

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import { mount } from 'enzyme';
 import CommentBox from 'components/CommentBox';
 
-if ('has a text area and a button', () => {
-    
+let wrapped;
+
+beforeEach(() => {
+    wrapped = mount(<CommentBox />);
+});
+
+afterEach(() => {
+    wrapped.unmount();
+})
+
+
+
+it ('has a text area and a button', () => {
+    expect(wrapped.find('textarea').length).toEqual(1);
+    expect(wrapped.find('button').length).toEqual(1);
 })
