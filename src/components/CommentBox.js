41f6292ce427e531d5ba303a44cc87ee4bc4c5d3
abe menashy <abestbet1@gmail.com>
2018-07-24 21:56:40 -0700

    add root component

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,4 +1,6 @@
 import React , {Component} from 'react';
+import { connect } from 'react-redux';
+import * as actions from 'actions';
 
 class CommentBox extends Component {
     state = { comment: ''}
@@ -8,6 +10,7 @@ class CommentBox extends Component {
 
     handleSubmit = (evt) => {
         evt.preventDefault();
+        this.props.saveComment(this.state.comment);
         this.setState({ comment: ''})
     }
     render(){
@@ -23,4 +26,4 @@ class CommentBox extends Component {
     }
 }
 
-export default CommentBox
+export default connect(null, actions)(CommentBox);
