f27a0bf87c1b736b8c84a29eb3d51e1a3c8be811
abe menashy <abestbet1@gmail.com>
2018-08-06 21:40:17 -0700

    test component html output

--- src/components/__tests__/CommentList.test.js
+++ src/components/__tests__/CommentList.test.js
@@ -18,5 +18,10 @@ beforeEach(() => {
 })
 
 it ('created one LI per comment', () => {
+    expect(wrapped.find('li').length).toEqual(2)
+})
 
+it ('shows the text for each comment', () => {
+    expect(wrapped.render().text()).toContain('Comment 1');
+    expect(wrapped.render().text()).toContain('Comment 2');
 })
